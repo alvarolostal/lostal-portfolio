c25c3acba44d363d6425231bfaa2b19be635d823
alvarolostal <alvarolostal04@gmail.com>
2025-10-19 14:36:02 +0200

    feat: Agregar integración de sitemap y actualizar metadatos SEO en Layout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://lostal.dev',
   output: 'static',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      entryLimit: 45000,
+    })
+  ],
   build: {
     inlineStylesheets: 'auto'
   }
