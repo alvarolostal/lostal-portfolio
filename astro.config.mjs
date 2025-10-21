3ce0691c98afd1c672be1ae69fda9e21bbb88192
alvarolostal <alvarolostal04@gmail.com>
2025-10-21 17:50:02 +0200

    chore: add @astrojs/sitemap and netlify-cli dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://lostal.dev',
   output: 'static',
-  integrations: [],
+  integrations: [sitemap()],
   build: {
     inlineStylesheets: 'auto',
   },
