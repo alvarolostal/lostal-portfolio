faa99bb592e87317f6f8d292134a50ef5a6b1f74
alvarolostal <alvarolostal04@gmail.com>
2025-10-21 17:34:59 +0200

    refactor: clean up package.json and optimize Layout.astro
    - Removed unused dependencies from package.json.
    - Improved formatting and consistency of comments in Layout.astro.
    - Ensured proper indentation and alignment for better readability.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://lostal.dev',
   output: 'static',
-  integrations: [
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-      entryLimit: 45000,
-    }),
-  ],
+  integrations: [],
   build: {
     inlineStylesheets: 'auto',
   },
